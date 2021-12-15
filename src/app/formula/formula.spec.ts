40972045447796b724355cfc505a79f6256f08e5
Yorielmonje <yoriel.monje.espidio.cet146@gmail.com>
2021-12-14 18:05:21 -0600

    formula

--- src/app/formula/formula.spec.ts
+++ src/app/formula/formula.spec.ts
@@ -7,20 +7,20 @@ describe ('Calcular',() => {
         form= new formula();
     });
     
-    it('Si la Frecuencia de la onda es 0, el resultado de longitud de onda debe ser 0',() =>{  
-        long=form.calcular(10,0);
+    it('Si mando 0 me debe regresar -17.7778 C',() =>{  
+        long=form.calcular(0);
+        expect(long).toBe(form.lon);
+    });
+    it('si mando 32 me debe regresar 0C',() =>{  
+        long=form.calcular(32);
         expect(long).toBe(0);
     });
-    it('Si la velocidad es -20 y la frecuencia de la onda es 4, la longitud de la onda debe ser -5',() =>{  
-        long=form.calcular(-20,4);
-        expect(long).toBe(-5);
+    it('si mando 100 me debe regresar 37.7778',() =>{  
+        long=form.calcular(100);
+        expect(long).toBe(form.lon);
     });
-    it('Si la velocidad es 60 y la frecuencia es -3, la longitud de la onda debe ser -20',() =>{  
-        long=form.calcular(60,-3);
-        expect(long).toBe(-20);
-    });
-    it('Si la Velocidad es 10 y la frecuencia de la onda 5, la longitud de la onda debe ser 2',() =>{  
-        long=form.calcular(10,5);
-        expect(long).toBe(2);
+    it('debe devolver el resultado',() =>{  
+        long=form.calcular(36);
+        expect(long).toBe(form.lon);
     });
 });
